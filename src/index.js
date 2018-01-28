13ed7bcc5434a8462c364782837bd87a5f7405dc
Josh Wasserman <github email address>
2018-01-27 21:41:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import shoppingListItemReducer from './reducers/shoppingListItemReducer';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import shoppingListItemReducer from "./reducers/shoppingListItemReducer";
+import App from "./App";
+import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+const store = createStore(shoppingListItemReducer);
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <Provider>
+    <App store={store} />
+  </Provider>,
+  document.getElementById("root")
 );
