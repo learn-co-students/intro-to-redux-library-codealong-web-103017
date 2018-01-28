292f69c2690eda732089fc938791d7a934f38197
github username <github email address>
2018-01-28 16:24:25 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
+
 class App extends Component {
+
+  handleClick = () =>
+  {
+    this.props.store.dispatch({
+      type: "INCREASE",
+    })
+  }
   render() {
     return (
       <div className="App">
-        App component
+        <button onClick={(event) => this.handleOnClick(event)} >
+         Click
+       </button>
+       <p>{this.props.store.getState().items.length}</p>
+
       </div>
     );
   }
